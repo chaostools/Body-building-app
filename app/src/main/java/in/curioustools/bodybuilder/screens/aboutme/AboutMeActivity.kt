39b51c48b7ae383fbfd9ee87c94df2a675de5848
root-ansh <anshsachdevaprofessional@gmail.com>
2020-06-01 21:46:13 +0530

    adding project files

---
+++ app/src/main/java/in/curioustools/bodybuilder/screens/aboutme/AboutMeActivity.kt
@@ -0,0 +1,12 @@
+package `in`.curioustools.bodybuilder.screens.aboutme
+
+import `in`.curioustools.bodybuilder.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AboutMeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_me)
+    }
+}
